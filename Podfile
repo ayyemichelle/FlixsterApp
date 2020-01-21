860551bca6b37332fce6f58851a6ad8ca076fe3e
ayyemichelle <michelle.v.aleman@gmail.com>
2020-01-21 12:04:03 -0800

    flix part 1

---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'flixsterApp' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for flixsterApp
+  # A pod is a library someone wrote: custom view, bluetooth, etc 
+  pod "AlamofireImage" 
+
+end
---
+++ flixsterApp/Assets.xcassets/MoviesViewController.swift
@@ -0,0 +1,84 @@
+//
+//  MoviesViewController.swift
+//  flixsterApp
+//
+//  Created by Michelle Vasquez-Aleman on 1/17/20.
+//  Copyright © 2020 Michelle Vasquez-Aleman. All rights reserved.
+//
+
+import UIKit
+import AlamofireImage
+
+
+class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+   
+    // properties - available for the lifetime of this screen
+    var movies = [[String:Any]]() // array of dictionaries - key: string, value: Any
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    // function run the first time the screen comes up
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { (data, response, error) in
+        // This will run when the network request returns
+        if let error = error {
+            print(error.localizedDescription)
+        } else if let data = data {
+            // data is contained in this dictionary
+            let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+            self.movies = dataDictionary["results"] as! [[String:Any]] // need to cast as array of dictionaries
+            
+            // need to tell tableView to update with the data we just retrieved
+            self.tableView.reloadData()
+            print(dataDictionary)
+            // TODO: Get the array of movies
+            // TODO: Store the movies in a property to use elsewhere
+            // TODO: Reload your table view data
+
+       }
+    }
+        task.resume()
+       
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count; // return how many rows
+    }
+    
+     // for this particular row, give me the cell
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+       
+    
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell") as! MovieCell
+        let movie = movies[indexPath.row]
+        let title = movie["title"] as! String
+        let synopsis = movie["overview"] as! String
+        cell.titleLabel?.text = title  // swift optionals + casting to String
+        cell.synopsisLabel.text = synopsis
+        
+        let baseUrl = "https://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        let posterUrl  = URL(string: baseUrl + posterPath)!
+        cell.posterView.af_setImage(withURL: posterUrl) // takes care of downloading and setting
+        return cell
+    }
+       
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
